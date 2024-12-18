c19be18359b4f8d4ef3c010b24376cce7287dce6
Настя <lekhno05@mail.ru>
2024-12-19 02:10:53 +0300

    perviy commit

---
+++ ImageSignProject-release/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:image_sign_project/home_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Подпись изображений',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),home: const HomeScreen(),
+    );
+  }
+}
